d0c8850f73795b40221ddff1342821d56325de98
Kurt <chrisronsmith@gmail.com>
2022-02-20 11:41:22 -0500

    brain dump

--- src/app.tsx
+++ src/app.tsx
@@ -21,11 +21,33 @@ type Event = { node: Id; newValue: number };
 // SECTION: LOGIC
 // ********************************************************
 
+/**
+ * Not built yet, but the idea is that the user will be able to update the graph by adding or removing nodes, and by writing formulas (for now just add expressions) that reference other nodes in the graph, creating dependent/dependency relationships between nodes. This web of dependent/dependency relationships will allow us to update the graph in a reactive way. When one node is updated, all of its dependents will be updated as well, and its dependents' dependents, and so on in a cascade.
+ *
+ * Managing those relationships will be a little fiddly, since both the dependent and the dependency need to be aware of each other. In concrete terms, we need to add the dependent to the dependency's 'dependents' list, and we need to add the dependency to the consuming node's 'addExpression.parts' list whenever we create or update a formula.
+ *
+ * This will all be handled by the appropriate branch of the reducer - the consuming code (tests, ui) will only be aware of certain events that they can dispatch, e.g. UPDATE_VALUE, UPDATE_FORMULA etc.
+ *
+ * Internally the reducer will have to to maintain certain invariants, such as:
+ *
+ * 1. "every node listed as a dependent is an expression, not a value"
+ * 2. "all relationships between nodes are known by both parties"
+ * 3. "the graph has no cycles"
+ *
+ * Number one could read, "all dependents are expressions".
+ * Number two means that if node A lists node B as an expression part, then node B must list node A as a dependent, and vice versa.
+ * Number three says it must be impossible to loop back onto a node that was already visited in the current path, because doing so would cause the reducer to enter an infinite loop.
+ *
+ * ...other requirements?
+ * */
 const reducer = (graph: Graph, event: Event): Graph => {
   const target = graph.find((node) => node.id === event.node);
 
   if (!target) return graph;
 
+  /**
+   * If I'm updating a dependent, it must be the case that it is an "addExpression" type node, since only expression types can depend on other nodes (rule is not implemented yet, but that's the plan). I'm providing a value to the newValue prop of the update event, but only because the types require it - the reducer will update the expression's value by re-evaluating the expression. This points to a potential improvement of the types - how do I make them better?
+   * */
   const consequentEvents: Event[] = target.dependents
     .map((dep) => ({
       type: "Update",
@@ -57,18 +79,6 @@ const reducer = (graph: Graph, event: Event): Graph => {
   }
 };
 
-const initialState: Graph = [
-  { id: "a", type: "value", value: 0, dependents: ["aPlusB"] },
-  { id: "b", type: "value", value: 0, dependents: ["aPlusB"] },
-  {
-    id: "aPlusB",
-    value: 0,
-    type: "addExpression",
-    addExpression: { parts: ["a", "b"] },
-    dependents: [],
-  },
-];
-
 // ********************************************************
 // SECTION: TEST
 // ********************************************************
@@ -79,8 +89,6 @@ const reducerTest = (
   expected: Graph,
 ) => {
   const actual: Graph = reducer(given.graph, given.event);
-
-  const stringify = (obj: object) => JSON.stringify(obj);
   const passed = deepequal(actual, expected);
 
   console.log(`
@@ -91,9 +99,9 @@ const reducerTest = (
     ${message}
     result: ${passed}
 
-    expected: ${stringify(expected)}
+    expected: ${JSON.stringify(expected)}
 
-    actual: ${stringify(actual)}
+    actual: ${JSON.stringify(actual)}
   `);
 };
 
@@ -213,9 +221,22 @@ for (const testCase of testCases) {
 // SECTION: UI
 // ********************************************************
 
+const initialState: Graph = [
+  { id: "a", type: "value", value: 0, dependents: ["aPlusB"] },
+  { id: "b", type: "value", value: 0, dependents: ["aPlusB"] },
+  {
+    id: "aPlusB",
+    value: 0,
+    type: "addExpression",
+    addExpression: { parts: ["a", "b"] },
+    dependents: [],
+  },
+];
+
 export function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // It would be nicer if I could get values without scanning the list, do a table lookup instead (object? hashMap? what data structure would be best for representing the graph?)
   const a = state.find((el) => el.id === "a");
   const b = state.find((el) => el.id === "b");
   const aPlusB = state.find((el) => el.id === "aPlusB");
@@ -249,6 +270,7 @@ export function App() {
             newValue: parseInt(event.currentTarget.value),
           })}
       />
+
       <label htmlFor="aPlusB">A + B</label>
       <input
         readOnly
